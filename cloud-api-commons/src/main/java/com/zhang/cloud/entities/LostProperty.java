863e3dc71f9469b3e81a3a4b03d48e03ecb41c2a
985492783 <985492783@qq.com>
2021-11-13 12:36:22 +0800

    Add: LostProperty

---
+++ cloud-api-commons/src/main/java/com/zhang/cloud/entities/LostProperty.java
@@ -0,0 +1,43 @@
+package com.zhang.cloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 98549
+ * @date 2021/11/12 0:50
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LostProperty {
+    /**
+     * 物品id
+     */
+    private Integer id;
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 信息
+     */
+    private String info;
+    /**
+     * 地点
+     */
+    private String location;
+    /**
+     * qq
+     */
+    private String qq;
+    /**
+     * wx
+     */
+    private String wx;
+    /**
+     * image
+     */
+    private String image;
+}

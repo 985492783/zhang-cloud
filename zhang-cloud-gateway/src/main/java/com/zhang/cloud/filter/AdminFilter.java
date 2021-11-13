4d90f1faf1882a1b29a9cc747ed3b0e78765c7bd
985492783 <985492783@qq.com>
2021-11-13 11:25:17 +0800

    Add: Admin Filter

--- zhang-cloud-gateway/src/main/java/com/zhang/cloud/filter/AdminFilter.java
+++ zhang-cloud-gateway/src/main/java/com/zhang/cloud/filter/AdminFilter.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -13,10 +15,15 @@ import reactor.core.publisher.Mono;
  * @author 98549
  * @date 2021/11/9 19:14
  */
+@Component
 public class AdminFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if(!isNeedFilter(request.getPath().value())){
+            return chain.filter(exchange);
+        }
         ServerHttpResponse response = exchange.getResponse();
         String token = exchange.getRequest().getHeaders().getFirst("token");
         String uid= (String) JWTUtil.parseToken(token).getPayload("uid");
@@ -29,7 +36,7 @@ public class AdminFilter implements GlobalFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return 0;
+        return 1;
     }
     public Boolean isNeedFilter(String uri){
         if(uri.contains("/api/admin")){
